96a5f394ef1c992f05f27778dfaf8d8daef3bc75
jeet solanki <solankijeet285@gmail.com>
2025-03-19 01:10:52 +0530

    initial commit

---
+++ src/main/java/com/jlss/placelive/userservice/PlaceLiveUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jlss.placelive.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceLiveUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceLiveUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JLSS/PlaceLive_User/PlaceLiveUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JLSS.PlaceLive_User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceLiveUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
